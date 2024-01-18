b30fade1bb20defdb7fa3f77d6f18eeca2df45cc
richard <richard>
2024-01-18 18:36:01 +0000

    add files

--- get-tweet-data/__test__/get-tweet-data.test.js
+++ get-tweet-data/__test__/get-tweet-data.test.js
@@ -1,1 +1,11 @@
-const getTweetData = require('../getTweetData')
+const getTweetData = require('../getTweetData.js')
+
+// TO DO LIST: 
+// It should return { tags: [], mentions: [], tagCount: 0, mentionCount: 0, length: 0 } if tweet string is empty
+// It should return the length of n for any string without a mention or a hashtag length n the tweet.
+// It should return the amount of hash tags ( #awesomeNorthcoders ) in the tweet
+// It should return an array of hash tags.
+// It should the amount of mentions ( @northcoders ) in the tweet 
+// It should return an array of mentions.
+// It should only want to return unique hash tags
+// It should handle mentioned.
